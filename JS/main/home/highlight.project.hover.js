dac4f687643c58bebf3fa3c316017579b6e6498d
mikaelRocha <mikaelrochadejesus@gmail.com>
2022-05-28 11:03:05 -0300

    NEW html css and javascript stamp

--- JS/main/home/highlight.project.hover.js
+++ JS/main/home/highlight.project.hover.js
@@ -5,9 +5,10 @@ const bigImage =document.getElementById('big-image-on-right');
 
 centerArticle.addEventListener('mouseover', (event)=>{
 
-    
+    if (event.target.id === 'first-highlight-li') 
+    {
     bigImage.style.opacity = 0.91
-
+    }
 })
 centerArticle.addEventListener('mouseout', (event)=>{
 
