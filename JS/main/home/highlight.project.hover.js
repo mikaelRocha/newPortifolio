efb50be4910299d8d15ac110f305a7d29e8cee52
mikaelRocha <mikaelrochadejesus@gmail.com>
2022-05-28 23:59:25 -0300

    REFAC html classes and files whith same name

--- JS/main/home/highlight.project.hover.js
+++ JS/main/home/highlight.project.hover.js
@@ -1,13 +1,13 @@
 
 const centerArticle = document.getElementById('highlight-center');
-const bigImage =document.getElementById('big-image-on-right');
+const bigImage =document.getElementById('highlight-big-image-on-right');
 
 
 centerArticle.addEventListener('mouseover', (event)=>{
 
     if (event.target.id === 'first-highlight-li') 
     {
-    bigImage.style.opacity = 0.91
+    bigImage.style.opacity = 1
     }
 })
 centerArticle.addEventListener('mouseout', (event)=>{
