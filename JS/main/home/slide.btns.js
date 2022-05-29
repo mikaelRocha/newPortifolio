d631353ad38ea4c93bfd278b4adb9537bf646601
mikaelRocha <mikaelrochadejesus@gmail.com>
2022-05-29 14:09:01 -0300

    BUG slide's bug rolling imagens fixed left side

--- JS/main/home/slide.btns.js
+++ JS/main/home/slide.btns.js
@@ -39,24 +39,29 @@ let booleanControl = 1
 
     if(event.target.id === "highlight-left")
     {
-        const comingSoonLi = document.getElementById('highlight-center').firstElementChild.firstElementChild
-        const cssObjSoon = window.getComputedStyle(comingSoonLi, null);
-        let   marginTop = cssObjSoon.getPropertyValue("margin-top")
-        
-        console.log(marginTop);
-
-        let marginTopCutted = marginTop.slice(0,-2);
-        marginTop = Number(marginTopCutted)
-
-        marginTop = marginTop + slidebtnLeft.getBoundingClientRect().height;
-        comingSoonLi.style.marginTop  = marginTop+"px";
-        
-        
-        console.log(marginTop);
-
-        if(marginTop > 0 )
+        if(booleanControl === 1)
         {
-            comingSoonLi.style.marginTop = "0"
+            booleanControl = 0
+            const comingSoonLi = document.getElementById('highlight-center').firstElementChild.firstElementChild
+            const cssObjSoon = window.getComputedStyle(comingSoonLi, null);
+            let   marginTop = cssObjSoon.getPropertyValue("margin-top")
+            
+            console.log(marginTop);
+
+            let marginTopCutted = marginTop.slice(0,-2);
+            marginTop = Number(marginTopCutted)
+
+            marginTop = marginTop + slidebtnLeft.getBoundingClientRect().height;
+            comingSoonLi.style.marginTop  = marginTop+"px";
+            
+            
+            console.log(marginTop);
+
+            if(marginTop > 0 )
+            {
+                comingSoonLi.style.marginTop = "0"
+            }
+            setTimeout(()=>{booleanControl = 1 }, 500)
         }
-    }
+}
 })
