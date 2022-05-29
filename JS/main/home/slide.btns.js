cfaa7ea5fede043340c6e4ad02930eead4b06be6
mikaelRocha <mikaelrochadejesus@gmail.com>
2022-05-29 13:36:47 -0300

    BUG slide's bug rolling imagens fixed

--- JS/main/home/slide.btns.js
+++ JS/main/home/slide.btns.js
@@ -1,30 +1,39 @@
- const slidebtnRight = document.getElementById('highlight-right')
- const slidebtnLeft = document.getElementById('highlight-left')
+const slidebtnRight = document.getElementById('highlight-right')
+const slidebtnLeft = document.getElementById('highlight-left')
 
- 
-//  document.defaultView.getComputedStyle(elemento,'').getPropertyValue("margin-left") 
-document.addEventListener("click", (event)=>
-{
- 
-    if(event.target.id === "highlight-right")
-    {
-        const comingSoonLi = document.getElementById('highlight-center').firstElementChild.firstElementChild
-        const cssObjSoon = window.getComputedStyle(comingSoonLi, null);
-        let   marginTop = cssObjSoon.getPropertyValue("margin-top")
-        console.log(marginTop);
-        let marginTopCutted = marginTop.slice(0,-2);
-        marginTop = Number(marginTopCutted)
+let booleanControl = 1
 
-        marginTop = marginTop - slidebtnLeft.getBoundingClientRect().height;
-        comingSoonLi.style.marginTop  = marginTop+"px";
-        //  marginTop = cssObjSoon.getPropertyValue("margin-top")
-
-        console.log(marginTop);
-
-        if(marginTop < -document.getElementById('highlight-center').firstElementChild.getBoundingClientRect().height)
+ //  document.defaultView.getComputedStyle(elemento,'').getPropertyValue("margin-left") 
+ document.addEventListener("click", (event)=>
+ {
+     
+     if(event.target.id === "highlight-right")
+     {
+        if(booleanControl === 1)
         {
-            comingSoonLi.style.marginTop = "0"
+            booleanControl = 0
+            const comingSoonLi = document.getElementById('highlight-center').firstElementChild.firstElementChild
+            const cssObjSoon = window.getComputedStyle(comingSoonLi, null);
+        
+            let   marginTop = cssObjSoon.getPropertyValue("margin-top")
+            const marginTopCutted = marginTop.slice(0,-2);
+            console.log(marginTop);
+            marginTop = Number(marginTopCutted)
+        
+            marginTop = marginTop - slidebtnLeft.getBoundingClientRect().height;
+            comingSoonLi.style.marginTop  = marginTop+"px";
+            //  marginTop = cssObjSoon.getPropertyValue("margin-top")
+    
+            console.log(marginTop);
+    
+            if(marginTop < -document.getElementById('highlight-center').firstElementChild.getBoundingClientRect().height)
+            {
+                comingSoonLi.style.marginTop = "0"
+            }
+            
+         setTimeout(()=>{booleanControl = 1 }, 500)
         }
+      
     }
 
 
