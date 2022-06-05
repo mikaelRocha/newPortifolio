2c80233b7f45aca8199a626c970a76a2bfc01cf2
mikaelRocha <mikaelrochadejesus@gmail.com>
2022-06-05 16:38:50 -0300

    NEW .mouse-follower removed, added .project-description

--- JS/mouseFollower.js
+++
@@ -1,8 +0,0 @@
-
-document.body.insertAdjacentHTML("afterbegin", "<div id='mouse-follower' class='mouse-follower'> </div>");
-
-let circle = document.getElementById('mouse-follower');const onMouseMove = (e) =>{
-    circle.style.left = e.pageX + 'px';
-    circle.style.top = e.pageY + 'px';
-  }
-document.addEventListener('mousemove', onMouseMove);
