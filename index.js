950dae85a748b53420ee13d3921ccb2c3ee9dbef
mikaelRocha <mikaelrochadejesus@gmail.com>
2022-06-16 14:45:21 -0300

    fix overflow text out of element

--- index.js
+++ index.js
@@ -9,20 +9,9 @@ let newUserInteractionController = new UserInteractionController(
     newUserInteractionModel,
     newUserInteractionview);
 
-let projectsUl = document.querySelector('.projects__ul');
-let projectsP  = document.querySelector('.projects__description .description');
-
-// newUserInteractionController.method01();
-
-projectsP.addEventListener('mouseenter',(event)=>
-{
-   newUserInteractionController.showProjectDescription(projectsUl)
-})
-
-projectsP.addEventListener('mouseout',(event)=>
-{
-    newUserInteractionController.hideProjectDescription(projectsUl)
-})
-
-
 newUserInteractionController.getProjects('http://127.0.0.1:5500/projects.json')
+newUserInteractionController.clickEventHandler()
+
+
+
+
--- userInteractionView.js
+++ userInteractionView.js
@@ -17,22 +17,33 @@ class UserInteractionView {
 
     showProjects(GET_Projects)
     {
-        let contentFields = document.querySelectorAll('[project]');
-        console.log(GET_Projects[0]);
-        console.log(contentFields);
-        
-        contentFields.forEach((element) =>
-    {
-        if(element.getAttribute('project') === "description" )
+        let projectsUl = document.querySelector('.projects__ul') 
+        console.log(GET_Projects);
+        GET_Projects.forEach((element,idx) =>
         {
-            element.textContent =   GET_Projects[0].description
-        }
-        if(element.getAttribute('project') === "technologys" )
-        {
-            element.textContent =   GET_Projects[0].technologys
-        }
-    })    
-    
+            projectsUl.insertAdjacentHTML("afterbegin",`  
+            <li class="projects__item">
+                <ul class="item__images">
+                    <li></li>
+                </ul>
+                <div class='project__description'>
+                    <article>
+                        <p class="description" project="description" >
+                            ${element.description}
+                        </p>
+                       <p class="technologys"  project="technologys"> 
+                            ${element.technologys}
+                       </p>
+
+                        <div class="projects__btns">
+                            <span class="projects__code"       project="code">      <a btn="code" href="${element.LlinkCode}">Code              </a>  </span>
+                            <span class="projects__application"project="aplication" ><a btn="application" href="${element.LlinkAplication}">Aplication</a>  </span>
+                        </div>
+                    </article>
+
+                </div>
+            </li>`)
+        })
     }
 }
 
